9a52a3b85d1ef28a78f057e93a8d935bf737fc8a
wabernathy96 <wabernathy96@gmail.com>
2018-04-03 21:03:47 -0500

    Spotify command logic works, booyah

--- liri.js
+++ liri.js
@@ -53,28 +53,60 @@ if(liriCmd === ''){
     // Logic for spotify-this-song command
     var sClient = new Spotify(spotifyKeys);
 
-    if (liriCmd === "spotify-this-song"){
-        var songArg = cmdArg[3];
+    if (liriCmd === 'spotify-this-song'){
+        
+        cmdArg.shift(); // Shift past node.js in node array
+        cmdArg.shift(); // Shift past file path in node array
+        cmdArg.shift(); // Shift past spotify command in node array
+
+        var songArg = cmdArg.join('-'); // Join all other words from [3] on
 
         var spotParams = {
             type: 'track',
             query: songArg,
+            limit: 1
         };
 
         if (songArg != ''){
             sClient.search(spotParams, function(err, data){
                 if(!err){
-                    console.log(data);
+                    for(j=0; j<data.tracks.items.length;j++){
+                        var topSong = data.tracks.items[j];
+                        
+                        var name = topSong.name;
+                        console.log(name);
+                        var artist = topSong.artists[0].name;
+                        console.log(artist);
+                        var album = topSong.album.name;
+                        console.log(album);
+                        var link = topSong.external_urls.spotify;
+                        console.log(link);
+                    };
                 }else{
                     throw err
                 }
-            })
-
+            });
         }else{
-            //default
-        }
-        
-    }
+            sClient.search({type: 'track', query: 'Baptized in Decadence'}, function(err, data){
+                if(!err){
+                    for(j=0; j<data.tracks.items.length;j++){
+                        var topSong = data.tracks.items[j];
+                        
+                        var name = topSong.name;
+                        console.log(name);
+                        var artist = topSong.artists[0].name;
+                        console.log(artist);
+                        var album = topSong.album.name;
+                        console.log(album);
+                        var link = topSong.external_urls.spotify;
+                        console.log(link);
+                    };
+                }else{
+                    throw err
+                }
+            });
+        }; 
+    };
     // Logic for movie-this command
 
     // Logic for do-what-it-says command
